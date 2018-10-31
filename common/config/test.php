ef26a512c02e919a5bd1cb964582f9a9fb28636c
Dmitriy Afanasev <afanavt@gmail.com>
2018-10-31 22:40:01 +0700

    настроил общие куки для всех доменов, тесты работают

--- common/config/test.php
+++ common/config/test.php
@@ -1,4 +1,8 @@
 <?php
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params-local.php'
+);
+
 return [
     'id' => 'app-common-tests',
     'basePath' => dirname(__DIR__),
@@ -6,6 +10,24 @@ return [
         'user' => [
             'class' => 'yii\web\User',
             'identityClass' => 'common\models\User',
+            'identityCookie' => [
+                'name' => '_identity',
+                'httpOnly' => true,
+                'domain' => $params['cookieDomain'],
+            ],
+        ],
+        'session' => [
+            'name' => '_session',
+            'cookieParams' => [
+                'domain' => $params['cookieDomain'],
+                'httpOnly' => true,
+            ],
+        ],
+        'urlManager' => [
+            'showScriptName' => true,
+        ],
+        'request' => [
+            'cookieValidationKey' => $params['cookieValidationKey'],
         ],
     ],
 ];
