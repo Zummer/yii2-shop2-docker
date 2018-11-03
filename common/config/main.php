6e86947ddc1125a3aa9d385da3403ee3dd73f847
Dmitriy Afanasev <afanavt@gmail.com>
2018-11-03 19:45:03 +0700

    Memcahce заработал

--- common/config/main.php
+++ common/config/main.php
@@ -2,13 +2,19 @@
 return [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@npm' => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\MemCache',
             'useMemcached' => true,
+            'servers' => [
+                [
+                    'host' => 'memcached_server',
+                    'port' => 11211,
+                ],
+            ],
         ],
     ],
 ];
